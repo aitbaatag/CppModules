62838d88dab1ab567f4ad5967d0fa4fa73324e88
Khalid Ait Baatag <kait-baa@e1r1p10.1337.ma>
2024-10-28 16:22:22 +0100

    ex01 ....

---
+++ Module-00/ex01/Contact.hpp
@@ -0,0 +1,27 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <string>
+
+class Contact {
+private:
+    std::string firstName;
+    std::string lastName;
+    std::string nickname;
+    std::string phoneNumber;
+    std::string darkestSecret;
+
+public:
+    void setFirstName(std::string name);
+    void setLastName(std::string name);
+    void setNickname(std::string name);
+    void setPhoneNumber(std::string number);
+    void setDarkestSecret(std::string secret);
+    std::string getFirstName();
+    std::string getLastName();
+    std::string getNickname();
+    std::string getPhoneNumber();
+    std::string getDarkestSecret();
+};
+
+#endif
---
+++ Module-00/ex01/contact.cpp
@@ -0,0 +1,41 @@
+#include "Contact.hpp"
+    void Contact::setFirstName(std::string name)
+    {
+        firstName = name;
+    }
+    void Contact::setLastName(std::string name)
+    {
+        lastName = name;
+    }
+    void Contact::setNickname(std::string name)
+    {
+        nickname = name;
+    }
+    void Contact::setPhoneNumber(std::string number)
+    {
+        phoneNumber = number;
+    }
+    void Contact::setDarkestSecret(std::string secret)
+    {
+        darkestSecret = secret;
+    }
+    std::string Contact::getFirstName()
+    {
+        return (firstName);
+    }
+    std::string Contact::getLastName()
+    {
+        return (lastName);
+    }
+    std::string Contact::getNickname()
+    {
+        return (nickname);
+    }
+    std::string Contact::getPhoneNumber()
+    {
+        return (phoneNumber);
+    }
+    std::string Contact::getDarkestSecret()
+    {
+        return (darkestSecret);
+    }
