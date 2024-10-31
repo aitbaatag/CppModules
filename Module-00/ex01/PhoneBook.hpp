c05f290a743a9c3a5073a513703f80e1d2ca2533
aitbaatag <aitbaatagkhalid@gmail.com>
2024-10-31 04:54:52 +0100

    push

--- Module-00/ex01/PhoneBook.hpp
+++ Module-00/ex01/PhoneBook.hpp
@@ -9,12 +9,18 @@
 #define SEARCH "SEARCH"
 #define ADD "ADD"
 #define EXIT "EXIT"
+#define GREEN "\e[1;32m"
+#define YELLOW "\e[1;33m"
+#define BLUE "\e[1;34m"
+#define RED "\e[1;31m"
+#define RESET "\e[0m"
 
 class PhoneBook
 {
 private:
     Contact contacts[8];
     int index;
+
 public:
     PhoneBook();
     void addContact(void);
