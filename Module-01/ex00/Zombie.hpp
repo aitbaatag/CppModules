5090ffe3365bc872def9ef68ca9bcbeaadfbfeb1
Khalid Ait Baatag <kait-baa@c3r9p3.1337.ma>
2024-11-01 16:31:43 +0100

    ex00 Module

--- Module-01/ex00/Zombie.hpp
+++ Module-01/ex00/Zombie.hpp
@@ -1,15 +1,20 @@
 #ifndef ZOMBIE_HPP
 #define ZOMBIE_HPP
 
-#include "Zombie.h"
+#include <iostream>
+#include <string.h>
 class Zombie
 {
-    private:
-        std::string name;
+private:
+	std::string name;
 
-    public:
-        Zombie(std::string name);
-        ~Zombie(void);
-        void announce(void);
+public:
+	Zombie(std::string name);
+	~Zombie(void);
+	void announce(void);
 };
+
+Zombie *newZombie(std::string name);
+void randomChump(std::string name);
+
 #endif
