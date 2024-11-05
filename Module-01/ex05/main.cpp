eb6e4c8ea0062de0570ce971d9d889ac86ec661a
aitbaatag <kait-baa@c3r7p3.1337.ma>
2024-11-05 19:14:43 +0100

    push

--- Module-01/ex05/main.cpp
+++ Module-01/ex05/main.cpp
@@ -4,10 +4,10 @@ int main()
 {
     Harl harl;
 
-    // harl.complain("DEBUG");
-    // harl.complain("INFO");
+    harl.complain("DEBUG");
+    harl.complain("INFO");
     harl.complain("WARNING");
-    // harl.complain("ERROR");
-    // harl.complain("INVALID");
+    harl.complain("ERROR");
+    harl.complain("INVALID");
     return 0;
 }
