64b3e67afb643f2f6e8f4b75551e7294478291a7
aitbaatag <aitbaatagkhalid@gmail.com>
2024-11-06 16:29:24 +0100

    push

--- Module-01/ex06/Harl.cpp
+++ Module-01/ex06/Harl.cpp
@@ -13,43 +13,39 @@ Harl::~Harl(void)
 void Harl::debug(void)
 {
     std::cout << "[ DEBUG ]" << std::endl;
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl << std::endl;
 }
 
 void Harl::info(void)
 {
     std::cout << "[ INFO ] " << std::endl;
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
 }
 
 void Harl::warning(void)
 {
     std::cout << "[ WARNING ] " << std::endl;
-    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl << std::endl;
 }
 
 void Harl::error(void)
 {
     std::cout << "[ ERROR ] " << std::endl;
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl << std::endl;
 }
 
 void Harl::Filter(std::string Slevel)
 {
     int level = map[Slevel];
 
-    std::cout << "Filtering level: " << Slevel << std::endl;
     switch (level)
     {
     case 1:
         Harl::debug();
-        break;
     case 2:
         Harl::info();
-        break;
     case 3:
         Harl::warning();
-        break;
     case 4:
         Harl::error();
         break;
