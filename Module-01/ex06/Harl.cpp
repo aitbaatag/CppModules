327bf15eaf7fb6750f2997952e53522f6884499e
Khalid Ait Baatag <kait-baa@e2r4p10.1337.ma>
2024-11-06 14:38:19 +0100

    push

--- Module-01/ex06/Harl.cpp
+++ Module-01/ex06/Harl.cpp
@@ -2,31 +2,35 @@
 
 Harl::Harl(void) 
 {
-    map["DEBUG"] = 0;
-    map["INFO"] = 1;
-    map["WARNING"] = 2;
-    map["ERROR"] = 3;
+    map["DEBUG"] = 1;
+    map["INFO"] = 2;
+    map["WARNING"] = 3;
+    map["ERROR"] = 4;
 }
 Harl::~Harl(void)
 {
 }
 void Harl::debug(void)
 {
+    std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info(void)
 {
+    std::cout << "[ INFO ] " << std::endl;
     std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void)
 {
+    std::cout << "[ WARNING ] " << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(void)
 {
+    std::cout << "[ ERROR ] " << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
@@ -37,16 +41,16 @@ void Harl::Filter(std::string Slevel)
     std::cout << "Filtering level: " << Slevel << std::endl;
     switch (level)
     {
-    case 0:
+    case 1:
         Harl::debug();
         break;
-    case 1:
+    case 2:
         Harl::info();
         break;
-    case 2:
+    case 3:
         Harl::warning();
         break;
-    case 3:
+    case 4:
         Harl::error();
         break;
     default:
