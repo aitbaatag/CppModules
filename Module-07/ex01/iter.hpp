4f99cb83ea6f38c0374f420359ca38f59c77faa0
aitbaatag <aitbaatagkhalid@gmail.com>
2025-10-25 17:17:58 +0100

    Push

--- Module-07/ex01/iter.hpp
+++ Module-07/ex01/iter.hpp
@@ -9,8 +9,8 @@ template <typename T> void iter(T *array, size_t length, void (*func)(T &ref)) {
   }
 }
 
-template <typename T>
-void iter(T const *array, size_t length, void (*func)(T const &ref)) {
+template <typename T, typename F>
+void iter(T const *array, size_t length, F func) {
   for (size_t i = 0; i < length; i++) {
     func(array[i]);
   }
--- Module-07/ex01/main.cpp
+++ Module-07/ex01/main.cpp
@@ -11,13 +11,13 @@ int main() {
   size_t length = sizeof(arr) / sizeof(arr[0]);
 
   std::cout << "Array elements: ";
-  iter(arr, length, printInt);
+  iter(arr, length, &printInt);
   std::cout << std::endl;
 
   // Test with const array
   const int constArr[] = {10, 20, 30, 40};
   std::cout << "Const array elements: ";
-  iter(constArr, 4, printConstInt);
+  iter<int>(constArr, 4, &printConstInt);
   std::cout << std::endl;
 
   return 0;
