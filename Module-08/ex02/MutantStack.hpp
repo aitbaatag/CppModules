bb5b3571d6344df564fa77bdf09375840f8ad956
aitbaatag <aitbaatagkhalid@gmail.com>
2025-10-27 19:05:15 +0100

    add more tests

--- Module-08/ex02/MutantStack.hpp
+++ Module-08/ex02/MutantStack.hpp
@@ -18,14 +18,18 @@ public:
       const_reverse_iterator;
 
   MutantStack() {}
+
   MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+
   MutantStack &operator=(const MutantStack &other) {
     if (this != &other) {
       std::stack<T>::operator=(other);
     }
     return *this;
   }
+
   ~MutantStack() {}
+
   iterator begin() { return this->c.begin(); }
 
   iterator end() { return this->c.end(); }
--- Module-08/ex02/main.cpp
+++ Module-08/ex02/main.cpp
@@ -2,8 +2,8 @@
 
 int main() {
   MutantStack<int> mstack;
-  mstack.push(5);
-  mstack.push(17);
+  mstack.push(1);
+  mstack.push(2);
 
   std::cout << mstack.top() << std::endl;
 
@@ -12,20 +12,36 @@ int main() {
   std::cout << mstack.size() << std::endl;
 
   mstack.push(3);
+  mstack.push(4);
   mstack.push(5);
-  mstack.push(737);
   //[...]
-  mstack.push(0);
+  mstack.push(6);
 
   MutantStack<int>::iterator it = mstack.begin();
   MutantStack<int>::iterator ite = mstack.end();
 
   ++it;
   --it;
+  std::cout << "\n";
+  std::cout << "\033[33m" << "Test with begin and end\n" << "\033[0m";
+
+  std::cout << "stack: " << std::endl;
   while (it != ite) {
     std::cout << *it << std::endl;
     ++it;
   }
   std::stack<int> s(mstack);
+
+  std::cout << "\n";
+  std::cout << "\033[33m" << "Test with rbegin and rend\n" << "\033[0m";
+
+  MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+  MutantStack<int>::reverse_iterator rite = mstack.rend();
+
+  std::cout << "stack: " << std::endl;
+  while (rit != rite) {
+    std::cout << *rit << " " << std::endl;
+    ++rit;
+  }
   return 0;
 }
